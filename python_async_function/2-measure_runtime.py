b6b743d1c06ab9ce6187ea23e3201e34833835e3
nikolag1jic93430-lab <nikolag1jic93430@gmail.com>
2026-04-09 13:03:26 +0200

    Refactor runtime measurement in async function
    Updated runtime measurement to use time.perf_counter for better accuracy.

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-"""Measure runtime of async execution"""
-import asyncio
 import time
-import importlib
+import asyncio
 
-wait_n = importlib.import_module('1-concurrent_coroutines').wait_n
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """Return average runtime per coroutine."""
-    start = time.time()
+    start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    return (time.time() - start) / n
+    end_time = time.perf_counter()
+    return (end_time - start_time) / n
