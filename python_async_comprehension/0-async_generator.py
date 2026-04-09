03db284f4e39f9c329937e6d749506554832cc9e
nikolag1jic93430-lab <nikolag1jic93430@gmail.com>
2026-04-09 13:12:51 +0200

    Change async_generator return type to Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-"""Async Generator"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """Yield 10 random floats asynchronously, waiting 1 second between each yield."""
+async def async_generator() -> Generator[float, None, None]:
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
