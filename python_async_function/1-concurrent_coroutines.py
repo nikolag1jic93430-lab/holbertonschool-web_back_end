5e9a0dcd22e79dc9f0795975a78bba2f64cd4d0c
nikolag1jic93430-lab <nikolag1jic93430@gmail.com>
2026-04-09 13:04:48 +0200

    Refactor wait_n to use updated wait_random import

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
-"""Module for concurrent coroutines."""
 import asyncio
-import importlib
 from typing import List
 
-wait_random = importlib.import_module('0-basic_async_function').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn wait_random n times and return delays in ascending order."""
-    tasks = [wait_random(max_delay) for _ in range(n)]
     delays = []
+    tasks = [wait_random(max_delay) for _ in range(n)]
 
     for task in asyncio.as_completed(tasks):
-        result = await task
-        delays.append(result)
+        delay = await task
+        delays.append(delay)
 
     return delays
