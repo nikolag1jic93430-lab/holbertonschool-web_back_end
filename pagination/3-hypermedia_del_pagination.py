779274328a700b2b436b68405538e06629c0725c
Nikola Gajic <vediumtv93@gmail.Com>
2026-04-23 14:18:11 +0200

    task

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-
+"""
+Deletion-resilient hypermedia pagination
+"""
 import csv
 import math
 from typing import List, Dict
 
 
 class Server:
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -24,6 +28,8 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
@@ -33,6 +39,14 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Return a dictionary containing the following key-value pairs:
+        - index: the current start index of the return page.
+            That is the index of the first item in the current page.
+        - next_index: the next index to query with.
+        - page_size: the current page size
+        - data: the actual page of the dataset to return
+        """
         assert index is not None
         assert page_size > 0
         indexed_dataset = self.indexed_dataset()
